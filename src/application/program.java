f38f4ceebdda087cc8e0a80df774e8b819270e65
William Ramos <contato.williamramos@outlook.com>
2021-02-03 23:52:58 -0300

    Inserindo dados na base - Retornando linhas afetadas

--- src/application/program.java
+++ src/application/program.java
@@ -1,29 +1,42 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import db.DB;
 
 public class program {
 
 	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("SELECT * FROM DEPARTMENT");
 			
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + " , " + rs.getString("Name"));
-			}
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)");
+			
+			st.setString(1, "Carl Purple");
+			st.setString(2, "Carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}catch (SQLException e) {
 			e.printStackTrace();
+		}catch (ParseException e) {
+			e.printStackTrace();
 		}finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}

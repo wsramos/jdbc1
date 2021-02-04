8941b358c5c4c862f9ee14fbaec2853dd08240bc
William Ramos <contato.williamramos@outlook.com>
2021-02-04 00:00:34 -0300

    Inserindo dados na base - Retornando Ids gerados

--- src/application/program.java
+++ src/application/program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -21,7 +23,8 @@ public class program {
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)");
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
 			st.setString(1, "Carl Purple");
 			st.setString(2, "Carl@gmail.com");
@@ -31,7 +34,16 @@ public class program {
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}else {
+				System.out.println("No rows affected");
+			}
+			
 		}catch (SQLException e) {
 			e.printStackTrace();
 		}catch (ParseException e) {
